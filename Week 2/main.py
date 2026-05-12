e2ac38bfbcc94a35c73066379e43a06a03603f55
Tanmay <itapetanmay103@gmail.com>
2026-05-12 18:59:07 +0530

    Learning Classes and Object in Python

--- Week 2/main.py
+++ Week 2/main.py
@@ -6,5 +6,30 @@ class MyClass:
     x = 5
 
 # Object
+"""
 P1 = MyClass()
 print(P1.x)
+"""
+
+# Delete Object
+"""
+del P1
+print(P1.x)
+"""
+
+# Multiple Objects
+
+P1 = MyClass()
+P2 = MyClass()
+P3 = MyClass()
+
+print(P1.x)
+print(P2.x)
+print(P3.x)
+
+# Pass - If class is empty for some reason, you add Pass into it
+
+class Person:
+    pass
+
+# __init__() Method
